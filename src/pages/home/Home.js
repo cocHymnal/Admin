c3ae4c3fef03007966e73b1c642226ff47d478e9
Highscore <highscoretechng@gmail.com>
2023-06-18 08:13:49 +0100

    original

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,7 +4,7 @@ import "./styles/home.css"
 export default function Home() {
 
   const homeContents = [
-    {id:1, title:"Registered Users",  number: 1 },
+    {id:1, title:"Registered Users",  number: 0 },
     {id:2, title:"Total Sales",  number: 0 },
     {id:3, title:"Total Affiliates",  number: 0 },
     {id:4, title:"Total Profit $",  number: 0 },
